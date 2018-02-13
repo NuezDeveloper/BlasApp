2f26ba95c7d047b323d206ea5e546c3423661f9a
Unknown <nuezbeta.4@gmail.com>
2018-02-13 17:33:44 -0600

    MenuBar
    agregando Menu de usuario

--- BlasPascal/app/src/main/java/blaspascal/com/blaspascal/BIENVENIDOS.java
+++ BlasPascal/app/src/main/java/blaspascal/com/blaspascal/BIENVENIDOS.java
@@ -1,13 +1,24 @@
 package blaspascal.com.blaspascal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BIENVENIDOS extends AppCompatActivity {
-
+Button btnh1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenidos);
+        btnh1 = (Button) findViewById(R.id.btn3);
+        btnh1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intento = new Intent(BIENVENIDOS.this,Noticias.class);
+                startActivity(intento);
+            }
+        });
     }
 }
--- BlasPascal/app/src/main/java/blaspascal/com/blaspascal/LoginActivity.java
+++ BlasPascal/app/src/main/java/blaspascal/com/blaspascal/LoginActivity.java
@@ -1,16 +1,26 @@
 package blaspascal.com.blaspascal;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+Button btnLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        btnLog = (Button) findViewById(R.id.btnLogin);
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intento = new Intent(LoginActivity.this,BIENVENIDOS.class);
+                startActivity(intento);
+            }
+        });
     }
 }
