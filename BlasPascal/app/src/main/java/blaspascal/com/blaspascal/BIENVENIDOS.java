f759616c6b711481507623ae80dbbc9649d95135
David <davidmezaibarra1995@gmail.com>
2018-02-13 16:20:32 -0700

    creacion de apartado de seleccion de hijo

--- BlasPascal/app/src/main/java/blaspascal/com/blaspascal/BIENVENIDOS.java
+++ BlasPascal/app/src/main/java/blaspascal/com/blaspascal/BIENVENIDOS.java
@@ -1,16 +1,13 @@
 package blaspascal.com.blaspascal;
 
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class BIENVENIDOS extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_bienvenidos);
     }
 }
