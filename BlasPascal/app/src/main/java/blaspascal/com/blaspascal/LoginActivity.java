04abd6003684acc9eadbd493d709a50566cb565f
David <davidmezaibarra1995@gmail.com>
2018-02-13 19:18:40 -0700

    fullscreen login

--- BlasPascal/app/src/main/java/blaspascal/com/blaspascal/LoginActivity.java
+++ BlasPascal/app/src/main/java/blaspascal/com/blaspascal/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
@@ -14,6 +15,9 @@ Button btnLog;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         btnLog = (Button) findViewById(R.id.btnLogin);
         btnLog.setOnClickListener(new View.OnClickListener() {
             @Override
