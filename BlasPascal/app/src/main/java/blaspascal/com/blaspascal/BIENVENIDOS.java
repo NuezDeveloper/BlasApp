e17615bab15fb697aafad4517acaf660bb4feb33
Unknown <nuezbeta.4@gmail.com>
2018-02-13 18:15:35 -0600

    Icono Hijo
    Agregando icono del hijo

--- BlasPascal/app/src/main/java/blaspascal/com/blaspascal/BIENVENIDOS.java
+++ BlasPascal/app/src/main/java/blaspascal/com/blaspascal/BIENVENIDOS.java
@@ -5,14 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class BIENVENIDOS extends AppCompatActivity {
-Button btnh1;
+ImageButton btnh1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenidos);
-        btnh1 = (Button) findViewById(R.id.btn3);
+        btnh1 = (ImageButton) findViewById(R.id.btn3);
         btnh1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
